4e9689a8d0c1bf1cf637b305fef1c8fc47e7ca6e
takashimonn7 <takashimoon7@gmail.com>
2024-10-06 16:04:05 -0600

    first commit

---
+++ scripts/textContent.js
@@ -0,0 +1,41 @@
+// El texto puede ser contenido en este archivo JavaScript.
+document.getElementById("text-section").innerHTML = `
+    <p>
+        La seguridad de la información es el conjunto de prácticas, políticas, procedimientos y 
+        tecnologías diseñadas para proteger la información y los sistemas informáticos contra 
+        accesos no autorizados, uso indebido, alteración, destrucción o divulgación. Su objetivo 
+        principal es garantizar la confidencialidad, integridad y disponibilidad de los datos y 
+        recursos informáticos.
+    </p>
+    `;
+
+
+document.getElementById("confidencialidad").innerHTML = `
+    <p> 
+        Confidencialidad
+    </p>
+
+    <p> 
+        Asegurar que solo las personas o sistemas autorizados puedan acceder a la información.
+    </p>
+`
+
+document.getElementById("integridad").innerHTML = `
+    <p> 
+        Integridad
+    </p>
+
+    <p> 
+        Garantizar que los datos sean precisos, completos y no hayan sido modificados de manera no autorizada.
+    </p>
+`
+
+document.getElementById("disponibilidad").innerHTML = `
+    <p> 
+        Disponibilidad
+    </p>
+
+    <p> 
+        Asegurar que la información y los sistemas estén disponibles para su uso cuando se necesiten.
+    </p>
+`
